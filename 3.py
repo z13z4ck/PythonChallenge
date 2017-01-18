bcc6c20b52dbf8fbf9b3cf59f16490af2fcc61b4
Aziz <z13z4ck@gmail.com>
2017-01-18 23:56:36 +0800

    solution for challenge 3
    somehow need exact three same character in between each small character

--- 3.py
+++ 3.py
@@ -3,11 +3,11 @@ import re
 total = ""
 # newchar = ''
 for line in f:
-	newchar = re.compile("(?<=[A-Z]{3})[a-z](?=[A-Z]{3})")
+	newchar = re.compile("[^A-Z]+[A-Z]{3}([a-z])[A-Z]{3}[^A-Z]+")
 	# newchar = re.compile("[A-Z]{3}([a-z])[A-Z]{3}")
 	val = newchar.match(line)
 	# if val is not None:
 	# print newchar.findall(line)
 	total += "".join(newchar.findall(line))
 print total
-f.close()
+f.close() 
