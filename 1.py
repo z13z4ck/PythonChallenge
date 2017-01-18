561bb21171fc0cfff94758103237fc5fb27ff128
Aziz <z13z4ck@gmail.com>
2017-01-18 22:46:21 +0800

    1st build
    3rd is still in WIP

---
+++ 1.py
@@ -0,0 +1,20 @@
+character = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."	 
+newchar = ''
+print character
+
+for i in range(0,len(character)):
+	# print character[i]
+	# print ord(character[i])
+	if (ord(character[i]) != 32):
+		if (ord(character[i]) != 46):
+			if (ord(character[i])> 120):
+				newchar += chr(ord(character[i])-120+96)
+			else:
+				newchar += chr(ord(character[i]) + 2)
+		else:
+			newchar += chr(ord(character[i]))
+	else:
+		newchar += chr(ord(character[i]))
+		
+
+print newchar
---
+++ 2.py
@@ -0,0 +1,16 @@
+"""
+	Python Challenge 02
+	python source code to isolate normal character from a bunch of random ASCII character
+"""
+f = open('2_character.txt','r')
+import re
+newchar = ''
+for line in f:
+	# print line
+	newchar += "".join(re.findall("[a-zA-Z]+",line))
+print "Output = " +newchar
+f.close()
+
+
+
+
