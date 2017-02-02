ded54df89a1bedfa7c6ee0d6e956d365fb045438
Aziz <z13z4ck@gmail.com>
2017-02-03 00:05:58 +0800

    still WIP. TBC
    ~_~)Y

--- 6.py
+++ 6.py
@@ -1,1 +1,11 @@
 # solution for challenge 6
+import re
+f = open(r'channel\90052.txt','r')
+print f.read()
+line = f.readline()
+f.close()
+# line = "Next nothing is 94191"
+# for line in f:
+	# l = re.search
+l = re.match("nothing is (\d+)",line)
+print l.group(1)
