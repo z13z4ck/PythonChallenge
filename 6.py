08fde7a343d4324e14c5a1cf5a17b0eb923eb9c0
Aziz <z13z4ck@gmail.com>
2017-02-05 08:39:19 +0800

    add pycharm support

--- 6.py
+++ 6.py
@@ -2,10 +2,10 @@
 import re
 f = open(r'channel\90052.txt','r')
 print f.read()
-line = f.readline()
+line = f.readline(1)
 f.close()
 # line = "Next nothing is 94191"
 # for line in f:
-	# l = re.search
-l = re.match("nothing is (\d+)",line)
-print l.group(1)
+    # l = re.search
+l = re.search("Next nothing is",line)
+print l
