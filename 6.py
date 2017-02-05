5dcb8f0c1650bb5059b9b5b19bfee4ed5c9c9cc7
Aziz <z13z4ck@gmail.com>
2017-02-05 16:13:10 +0800

    solution for challenge 6

--- 6.py
+++ 6.py
@@ -1,11 +1,31 @@
 # solution for challenge 6
-import re
+import re,zipfile
 f = open(r'channel\90052.txt','r')
-print f.read()
-line = f.readline(1)
+print f.read().decode("utf-8")
+line = f.read().decode("utf-8")
 f.close()
 # line = "Next nothing is 94191"
 # for line in f:
     # l = re.search
-l = re.search("Next nothing is",line)
+l = re.search("Next nothing is (\D+)",line)
 print l
+
+comments=[]
+
+
+
+# simple and better
+print "2nd try"
+f = zipfile.ZipFile("channel.zip")
+
+currentnum = 90052
+while True:
+    content = f.read(str(currentnum) + '.txt').decode('utf-8')
+    print content
+    comments.append(f.getinfo(str(currentnum)+'.txt').comment.decode('utf-8'))
+    match = re.search("Next nothing is (\d+)",content)
+    try:
+        currentnum = match.group(1)
+    except Exception:
+        print "".join(comments)
+        break
